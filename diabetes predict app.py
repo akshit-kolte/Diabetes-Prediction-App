32a09fc35e93f3c19d6bb291f8f888e3a35b9c8e
akshit-kolte <akshitkolte9026@gmail.com>
2025-12-03 11:57:18 +0530

    Add files via upload

---
+++ diabetes predict app.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import joblib
+import streamlit as st
+#Load the Model
+
+model=joblib.load(open("logistic_regression_model (1).joblib", 'rb'))
+
+st.title("Diabetes Prediction app")
+#Input feature
+Age=st.number_input("Age of the person",min_value=0.0)
+BMI=st.number_input("BMI",min_value=0.0)
+BloodPressure=st.number_input("Newspaper Adv Budget",min_value=0.0)
+Insulin=st.number_input("Insulin",min_value=0.0)
+Glucose=st.number_input("Glucose",min_value=0.0)
+SkinThickness=st.number_input("SkinThickness",min_value=0.0)
+DiabetesPedigreeFunction=st.number_input("DiabetesPedigreeFunction",min_value=0.0)
+Pregnancies=st.number_input("Pregnancies",min_value=0.0)
+
+#Make Pred
+if st.button('Diabetes Prediction'):
+	input_data=np.array([[Age,BMI,BloodPressure,Insulin,Glucose,SkinThickness,DiabetesPedigreeFunction,Pregnancies]])
+	prediction_model=model.predict(input_data)[0]
+	st.success(f'Predict Diabetes:{Prediction:.2f}')
