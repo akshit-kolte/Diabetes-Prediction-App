a5ad2f36d1410323015075a519b2e2f62416c028
akshit-kolte <akshitkolte9026@gmail.com>
2025-12-03 12:03:54 +0530

    Update diabetes predict app.py

--- diabetes predict app.py
+++ diabetes predict app.py
@@ -4,21 +4,21 @@ import joblib
 import streamlit as st
 #Load the Model
 
-model=joblib.load(open("logistic_regression_model (1).joblib", 'rb'))
+model=joblib.load(open("logistic_regression_model (3).joblib", 'rb'))
 
 st.title("Diabetes Prediction app")
 #Input feature
-Age=st.number_input("Age of the person",min_value=0.0)
-BMI=st.number_input("BMI",min_value=0.0)
-BloodPressure=st.number_input("Newspaper Adv Budget",min_value=0.0)
-Insulin=st.number_input("Insulin",min_value=0.0)
-Glucose=st.number_input("Glucose",min_value=0.0)
-SkinThickness=st.number_input("SkinThickness",min_value=0.0)
-DiabetesPedigreeFunction=st.number_input("DiabetesPedigreeFunction",min_value=0.0)
 Pregnancies=st.number_input("Pregnancies",min_value=0.0)
+Glucose=st.number_input("Glucose",min_value=0.0)
+BloodPressure=st.number_input("BloodPressure",min_value=0.0)
+SkinThickness=st.number_input("SkinThickness",min_value=0.0)
+Insulin=st.number_input("Insulin",min_value=0.0)
+BMI=st.number_input("BMI",min_value=0.0)
+DiabetesPedigreeFunction=st.number_input("DiabetesPedigreeFunction",min_value=0.0)
+Age=st.number_input("Age",min_value=0.0)
 
 #Make Pred
 if st.button('Diabetes Prediction'):
-	input_data=np.array([[Age,BMI,BloodPressure,Insulin,Glucose,SkinThickness,DiabetesPedigreeFunction,Pregnancies]])
-	prediction_model=model.predict(input_data)[0]
-	st.success(f'Predict Diabetes:{Prediction:.2f}')
+	input_data=np.array([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+	prediction=model.predict(input_data)[0]
+	st.success(f'Predict Diabetes:{prediction:.2f}')
